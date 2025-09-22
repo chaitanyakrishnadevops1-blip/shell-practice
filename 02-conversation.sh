817c6bddca4af454558a28d9be0e150e7c6deecf
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-09-22 22:55:10 +0530

    some message

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh:: hi suresh, how are you?"
+echo  "suresh:: Hello Ramesh ], iam fine. How are you?"
+ech "ramesh::what are you learning recentliy?"
+echo "suresh:: iam learning shell scripting Ramesh"
