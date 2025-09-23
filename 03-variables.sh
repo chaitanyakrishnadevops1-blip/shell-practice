2e13abfd4023940bb164748a3d3ffb0eeeede568
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-09-23 17:45:45 +0530

    some message

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+
+# no space befor or after equal
+person1=john
+person2=Raheem
+
+echo "$person1:: hi $person2, how are you?"
+echo  "$person2:: Hello  $person1, iam fine. How are you?"
+ech "$person1::what are you learning recentliy?"
+echo "$person2:: iam learning shell scripting $person1"
+echo "$person1:: ok good"
