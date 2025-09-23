7d0fff37271ab0ce5feb516a008cb5d2e1ec8a43
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-09-23 21:41:49 +0530

    some message

---
+++ 04-textvariables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+
+PERSON1=$1
+PERSON1=$2
+
+echo "$PERSON1:: welcome to Alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not an zoom webinar"
+echo "$PERSON3:: so, do we have a deal"
+echo "$PERSON2:: yes, if you make ukraine joins russia"
+echo "$person1:: yes"
