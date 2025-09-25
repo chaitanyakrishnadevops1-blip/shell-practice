1db993eec4b0eb9a68f0d98278aabc5f9d821247
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-09-25 20:33:11 +0530

    some message

---
+++ 12.installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+      echo "ERROR:: please run this script with root privelege"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR":: Installing MYSQL is failure"
+else
+     echo "Installing MYSQL is SUCCESS"
