5916e4dfa84e841acf2a4947f2c41e422e46ee81
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-09-25 20:03:52 +0530

    some message

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "please enter the number:"
-read Number
+echo "please enter the NUMBER:"
+read NUMBER
 
 if [ $(($NUMBER % 2))  -eq 0 ]; then
    echo "Given number $NUMBER is EVEN"
 else 
-   echo "Given number $NUMBER is OOD"
+   echo "Given number $NUMBER is ODD"
 fi
