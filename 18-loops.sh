afdfe10ba429bf2cd27ab43f2c5a1888fd23a861
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-09-29 20:00:43 +0530

    some message

---
+++ 18-loops.sh
@@ -0,0 +1,37 @@
+!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+ LOGS_FOLDER="/Var/log/shell-script"
+ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p "/var/log/shell-script"
+echo "Script started executed at: $(date)"
+
+if [ $USERID -ne 0 ]; then
+      echo "ERROR:: please run this script with root privelege"
+      exit 1 # failure is other than 0
+
+fi
+
+ VALIDATE(){
+     if [ $1 -ne 0 ]; then
+    echo -e "Installing $2....$R is failure $N" | tee -a$LOG_FILE
+    exit 1
+else
+     echo  -e "Installing $2 ...$G is SUCCESS $N" | tee -a $LOG_FILE
+
+  fi
+
+}
+# $@
+
+ for package  in $@
+ do 
+    echo "package is: $package"
+ done    
--- 19-logs.sh
+++ 19-logs.sh
@@ -21,11 +21,11 @@ fi
 
  VALIDATE(){
      if [ $1 -ne 0 ]; then
-    echo -e "Installing $2....$R is failure $N"
+    echo -e "Installing $2....$R is failure $N" | tee -a$LOG_FILE
     exit 1
 else
-     echo  -e "Installing $2 ...$G is SUCCESS $N"
-  fi
+     echo  -e "Installing $2 ...$G is SUCCESS $N" | tee -a$LOG_FILE
+  fi 
 
 }
 
