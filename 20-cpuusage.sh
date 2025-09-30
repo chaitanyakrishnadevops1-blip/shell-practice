573fa33571f8a0138889c4154e402e3013dcc758
chaitanya krishna <chaitanyakrishnadevops1@gmail.com>
2025-09-30 22:18:00 +0530

    some message

--- 20-cpuusage.sh
+++ 20-cpuusage.sh
@@ -2,12 +2,15 @@
 
 DISK_USAGE=$(df -ht | grep -v Filesystem)
 DISK_THRESHOLD=2 # IN PROJECT WE KEEP ITS AS 75
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 )
+
 
 while IFS= read -r line
 do
    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
    PARTITION=$(echo $line | awk '{print $7}')
    if [ $USAGE -ge $DISK_THRESHOLD ]; then
-   echo "High usage on $PARTITON: $USAGE"
+       MESSAGE="High usage on $PARTITON: $USAGE %"
    fi
 done <<< $DISK_USAGE
+echo "Message Body: $MESSAGE"
